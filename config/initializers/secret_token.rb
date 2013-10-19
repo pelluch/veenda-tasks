fa12b50254850274fcfa4a9ee35dbc98d0dd41cc
Pablo Lluch <pablo.lluch@gmail.com>
2013-10-18 22:11:49 -0300

    Create initial application with configured postgresql database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VeendaTasks::Application.config.secret_token = '8b97f5846469aed969527912a920ff0cd65a660a9fd58533b136246329a3a6c0545500596a009269ce06273721c8854a9d70946aa059cded51a1779ba6ebbf9c'
