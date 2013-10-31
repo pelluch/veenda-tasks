6c213484c7d7c9639ca7e5599f5fa4c8a0ed5ed8
Pablo Lluch <pablo.lluch@gmail.com>
2013-10-30 23:50:05 -0300

    Move Kanbanery module to lib folder, remove requires in column model #1138211

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,5 +1,4 @@
-require 'kanbanery'
+
 class Column < ActiveRecord::Base
-	include Kanbanery
 	attr_accessible :project_id
 end
---
+++ lib/kanbanery.rb
@@ -0,0 +1,5 @@
+module Kanbanery
+	SITE = "https://kanbanery.com/api/v1"
+	WORKSPACE = "iic215420132"
+	PROJECT_ID = "39793"
+end
