3bbcda58ac3c8a8f7713f501d1e9c050a1abfc78
Pablo Lluch <pablo.lluch@gmail.com>
2013-11-02 21:30:48 -0300

    Clean up routes, check for resource existence on kanbanery callbacks #1138211

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -15,19 +15,49 @@ class UpdatesController < ApplicationController
 	# DELETE
 	def destroy		
 		puts params
+		type = params["resources"]["type"]
+		resource = get_resource(type)
+		if resource
+			puts "The resource #{type} exists"
+		else
+			puts "The resource #{type} DOES NOT exist"
+		end
 		render json: { :status => "ok" }, status: :ok
 	end
 
 	# POST
 	def create		
 		puts params
+		type = params["resources"]["type"]
+		resource = get_resource(type)
+		if resource
+			puts "The resource #{type} exists"
+		else
+			puts "The resource #{type} DOES NOT exist"
+		end
 		render json: { :status => "ok" }, status: :ok
 	end
 
 	# PUT
 	def update		
 		puts params
+		type = params["resources"]["type"]
+		resource = get_resource(type)
+		if resource
+			puts "The resource #{type} exists"
+		else
+			puts "The resource #{type} DOES NOT exist"
+		end
 		render json: { :status => "ok" }, status: :ok	
 	end
 
+	private
+
+	def get_resource(type)
+		if Module.const_defined?(type)
+			Object.const_get(type)
+		else
+			nil
+		end
+	end
 end
